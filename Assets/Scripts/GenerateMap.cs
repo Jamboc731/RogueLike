508e2eb60668208c9fc74080ce066691e85e190d
JamieCovell <Jamiec.covellj@gmail.com>
2019-02-22 13:17:57 +0000

    started GenerateMap.cs

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class GenerateMap
+{
+
+
+
+	public Color[][] GetMap(int w, int h)
+    {
+        Color[][] map = new Color[w][];
+        for (int i = 0; i < map.Length; i++)
+        {
+            map[i] = new Color[h];
+        }
+
+
+
+        return map;
+    }
+
+    private int[][] CreateArray(int w, int h)
+    {
+        int[][] array = new int[w][];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new int[h];
+        }
+
+        return array;
+
+    }
+
+    private int[][] Propagate(int[][] _map)
+    {
+        int[][] map = _map;
+
+
+
+        return map;
+    }
+
+    private int[][] Backtrack(int[][] _map)
+    {
+        int[][] map = _map;
+
+
+        return map;
+    }
+
+    private void /*Vector2*/ LookAroundForAvailableSpaces(int[][] _map, int[][] _pos)
+    {
+
+    }
+
+}
