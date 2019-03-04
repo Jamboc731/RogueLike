951c9442a722a86ef74f1f94cd511b436abf995b
JakeusMeakus <JM219785@falmouth.ac.uk>
2019-03-04 22:33:17 +0000

    Wehh
    jamies going home

---
+++ Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    int maxHealth;
+    int currentHealth;
+
+    public int GetMax()
+    {
+        return maxHealth;
+    }
+
+    public int GetCurrent()
+    {
+        return currentHealth;
+    }
+
+    public void MaxHealth()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int value)
+    {
+        currentHealth -= value;
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    public void HealDamage(int value)
+    {
+        currentHealth += value;
+        if (currentHealth >= maxHealth)
+            MaxHealth();
+    }
+
+    protected virtual void Die()
+    {
+        Debug.Log(string.Format("{0} has died.", gameObject.name));
+    }
+
+}
