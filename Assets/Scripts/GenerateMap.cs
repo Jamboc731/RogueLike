271524370315ff1c16555837cfd669afe2f325db
JamieCovell <jamiec.covellj@gmail.com>
2019-03-11 23:42:48 +0000

    room gen iteration 1 works

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateMap
+public class GenerateMap : MonoBehaviour
 {
 
-    Vector2[] directions = new Vector2[] { new Vector2(0, 1), new Vector2(0, -1), new Vector2(1, 0), new Vector2(-1, 0) };
+    [SerializeField] int worldHalfExtents;
+    Room [,] world;
+    Vector2 currentPos;
+    List<Vector2> takenPositions = new List<Vector2> ();
+    Vector2 [] compass = new Vector2 [] { Vector2.up, Vector2.down, Vector2.right, Vector2.left };
+    [SerializeField] GameObject cub;
+    [SerializeField] int numberOfRooms;
+
+    List<Room> viablePostions = new List<Room> ();
+
+    int roomGenAttempt = 0;
+    const int GenAttemptMax = 10;
+
+    #region FirstMethod
+    /*
+    Vector2 [] directions = new Vector2[] { new Vector2(0, 1), new Vector2(0, -1), new Vector2(1, 0), new Vector2(-1, 0) };
 
 	public Color[][] GetMap(int w, int h)
     {
@@ -65,6 +81,202 @@ public class GenerateMap
         }
         
         return availableSpaces.ToArray();
+    }
+    */
+    #endregion
+
+    private void Start ()
+    {
+        GeneratWorldArray ();
+        PopulateWorld ();
+        //if (world [worldHalfExtents, worldHalfExtents] == null)
+        //    Debug.Log ("true");
+        //else Debug.Log ("False");
+        DisplayWorld ();
+
+    }
+
+    private void DisplayWorld ()
+    {
+        //Debug.Log (takenPositions.Count);
+        foreach (Room r in world)
+        {
+            //Debug.Log (world.Length);
+            if (r != null)
+                Instantiate (cub, r.GetPosition (), Quaternion.identity);
+        }
+
+    }
+
+    private void PopulateWorld ()
+    {
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+            int roomcount = 0;
+            foreach (Room r in world)
+            {
+                if (r != null)
+                {
+                    roomcount++;
+                    if (GetNeighbourCount (r) <= 2)
+                    {
+                        Debug.Log ("correct number of neighbours");
+                        viablePostions.Add (r);
+                    }
+                }
+            }
+            Debug.Log (i + "viable count " + viablePostions.Count);
+            MakeNeighbour (viablePostions[UnityEngine.Random.Range(0, viablePostions.Count - 1)].GetPosition(), 1);
+            viablePostions = new List<Room> ();
+        }
+    }
+
+    #region GetNeighbourCount (2 overloaded methods)
+    private int GetNeighbourCount (Room r)
+    {
+
+        int count = 0;
+        try
+        {
+            if (world [Mathf.RoundToInt (r.GetPosition ().x + Vector2.up.x), Mathf.RoundToInt (r.GetPosition ().y + Vector2.up.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range up");
+        }
+        try
+        {
+            if (world [Mathf.RoundToInt (r.GetPosition ().x + Vector2.down.x), Mathf.RoundToInt (r.GetPosition ().y + Vector2.down.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range down");
+        }
+        try
+        {
+            if (world [Mathf.RoundToInt (r.GetPosition ().x + Vector2.left.x), Mathf.RoundToInt (r.GetPosition ().y + Vector2.left.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range left");
+        }
+        try
+        {
+            if (world [Mathf.RoundToInt (r.GetPosition ().x + Vector2.right.x), Mathf.RoundToInt (r.GetPosition ().y + Vector2.right.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range right");
+        }
+        Debug.Log (count);
+        return count;
+
+    }
+
+    private int GetNeighbourCount (Vector2 pos)
+    {
+
+        int count = 0;
+        try
+        {
+            if (world [Mathf.RoundToInt (pos.x + Vector2.up.x), Mathf.RoundToInt (pos.y + Vector2.up.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range up");
+        }
+        try
+        {
+            if (world [Mathf.RoundToInt (pos.x + Vector2.down.x), Mathf.RoundToInt (pos.y + Vector2.down.y)] != null)
+                count++;                                                                         
+        }                                                                                        
+        catch                                                                                    
+        {                                                                                        
+            Debug.Log ("Out of world range down");                                               
+        }                                                                                        
+        try                                                                                      
+        {                                                                                        
+            if (world [Mathf.RoundToInt (pos.x + Vector2.left.x), Mathf.RoundToInt (pos.y + Vector2.left.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range left");
+        }
+        try
+        {
+            if (world [Mathf.RoundToInt (pos.x + Vector2.right.x), Mathf.RoundToInt (pos.y + Vector2.right.y)] != null)
+                count++;
+        }
+        catch
+        {
+            Debug.Log ("Out of world range right");
+        }
+        Debug.Log (count);
+        return count;
+
+    }
+    #endregion
+
+    private void GeneratWorldArray ()
+    {
+
+        world = new Room [worldHalfExtents * 2, worldHalfExtents * 2];
+
+        world [worldHalfExtents, worldHalfExtents] = new Room (new Vector2 (worldHalfExtents, worldHalfExtents), 0);
+
+        TakePosition (new Vector2 (worldHalfExtents, worldHalfExtents));
+
+    }
+
+    private void TakePosition (Vector2 pos)
+    {
+        takenPositions.Add (pos);
+    }
+
+    private void MakeNeighbour (Vector2 _pos, int type)
+    {
+        roomGenAttempt = 0;
+        RecursiveNeighbourLoop (_pos, type);
+    }
+
+    private void RecursiveNeighbourLoop (Vector2 _pos, int type)
+    {
+        if (roomGenAttempt >= GenAttemptMax) { Debug.Log ("Limit reached"); return; }
+        else
+        {
+            roomGenAttempt++;
+            try
+            {
+                _pos += compass [UnityEngine.Random.Range (0, 4)];
+                Vector2 pos = new Vector2 (Mathf.RoundToInt (_pos.x), Mathf.RoundToInt (_pos.y));
+                Debug.Log (pos);
+                if (!takenPositions.Contains (pos))
+                {
+                    if (GetNeighbourCount (pos) == 1)
+                    {
+                        world [Mathf.RoundToInt (pos.x), Mathf.RoundToInt (pos.y)] = new Room (pos, 1);
+                        TakePosition (pos);
+
+                    }
+                    else RecursiveNeighbourLoop (_pos, type);
+
+                }
+                return;
+            }
+            catch
+            {
+                RecursiveNeighbourLoop (_pos, type);
+                return;
+            }
+
+        }
+
     }
 
 }
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+
+    Vector2 worldPos;
+    int roomType;
+    bool [] doors;
+
+    public Room(Vector2 pos, int type)
+    {
+        worldPos = pos;
+        roomType = type;
+    }
+
+    public Vector2 GetPosition ()
+    {
+        return worldPos;
+    }
+
+}
